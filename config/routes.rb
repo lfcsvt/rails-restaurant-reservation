9e5a76173f38f7bb560a8f36c1d076c1901e7fda
lucas souza <lfcsvt@gmail.com>
2016-11-25 20:27:28 +0100

    gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+
+  get 'restaurants/find'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
+  get 'restaurants/update'
+
+  get 'restaurants/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
